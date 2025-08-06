79ac87ea91e4ed69bff3e9e6c1ff861013c4f489
Abdullahi Abdiwali <abdullahia@uia.no>
2025-08-06 16:17:27 +0200

    Updatet Firebase config

--- transport-monitor/src/firebase.js
+++ transport-monitor/src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";  // Importer Firestore
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCzpBG-Rr_1DqaG8CViWg7q4O2nsA1O994",
-  authDomain: "transport-monitoring-88175.firebaseapp.com",
-  projectId: "transport-monitoring-88175",
-  storageBucket: "transport-monitoring-88175.appspot.com",
-  messagingSenderId: "72012108989",
-  appId: "1:72012108989:web:125511e98aca4a698153d4",
-  measurementId: "G-MV3NX1LE8J"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
